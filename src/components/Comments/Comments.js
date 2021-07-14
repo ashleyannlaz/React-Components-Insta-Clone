16477010c8c7a5a9239aabd4ce1dc8dc78400762
ashleyannlaz <paperpastels@gmail.com>
2021-07-13 21:34:05 -0400

    Finished Project

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -1,7 +1,8 @@
 import React from 'react';
+// import Posts from '../Posts/Posts';
 import Comment from './Comment';
 import './Comments.css';
-
+//console.log(comments)
 const Comments = props => {
   // 🔥 Make sure the parent of Comments is passing the right props!
   const { comments } = props;
@@ -9,6 +10,16 @@ const Comments = props => {
   return (
     <div>
       {/* map through the comments prop and render a Comment for every piece of data */}
+      {/* {Posts.map((postObj) => {
+        return postObj.comments
+      })} */}
+      {/* {comments.map(comments => {
+        return <Comment comments={posts} key={comments.id}/>
+      })} */}
+    {comments.map(comment => {
+        return <Comment comment={comment} key={comment.id} />
+      })}
+      
     </div>
   );
 };
